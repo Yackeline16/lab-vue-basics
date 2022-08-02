5e75464c121783d76438d47f25eaaf10df36837a
Yackeline16 <jacqueline@capa8.net>
2022-08-02 21:04:59 +0200

    NavBar

---
+++ lab-basics/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar-a">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+  </nav>
+    <router-view/>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+    component: {
+        NavBar
+    }
+
+}
+</script>
+
+<style>
+.navbar-a{
+    background-color: aqua;
+    border: solid 2px black;
+}
+
+</style>
